10b0498ca2ec43de031eb7f6719c651a17863082
PatrykGeltz <patryk.geltz07@gmail.com>
2024-10-15 10:58:58 +0200

    added basic buttons dedicated to modules

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -6,9 +6,59 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hello World!'),
+    // Lista nazw przycisków
+    final List<String> buttonLabels = [
+      'Administracja',
+      'Organizacja',
+      'Kadry',
+      'Automatyzacja',
+      'Członkowie',
+      'Składki',
+      'Uprawnienia żeglarskie',
+      'Marina',
+      'Statusy',
+      'Ustawienia',
+    ];
+
+    return Scaffold(
+      // Ustawienie białego tła
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          // Nagłówek
+          const Padding(
+            padding: EdgeInsets.only(left: 16.0, bottom: 0.0, right: 16.0, top: 40.0),
+            child: Text(
+              'D360',
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 10.0,
+                  mainAxisSpacing: 10.0,
+                ),
+                itemCount: buttonLabels.length,
+                itemBuilder: (context, index) {
+                  return ElevatedButton(
+                    onPressed: () {
+                      // Obsługa kliknięcia przycisku
+                      print('${buttonLabels[index]} clicked');
+                    },
+                    child: Text(buttonLabels[index]),
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
