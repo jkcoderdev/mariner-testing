2f2128fb2d913235f29a941d2799c37bd62bd288
praktykant3 <dawid.domaniecki2@gmail.com>
2024-10-16 14:46:15 +0200

    Updated subsections list

--- lib/components/section_list.dart
+++ lib/components/section_list.dart
@@ -3,11 +3,12 @@ import 'section_button.dart';
 
 const Map < String, List > kSubsections = {
   '/administracja': <Widget>[
-    SectionButton(icon: Icons.accessibility, title: 'Witold'),
-    SectionButton(icon: Icons.abc_sharp, title: 'Warta')
+    SectionButton(icon: Icons.groups, title: 'Użytkownicy'),
+    SectionButton(icon: Icons.mail, title: 'Zaproszenia'),
+    SectionButton(icon: Icons.add_card_rounded, title: 'Uprawnienia'),
   ],
-  '/kadry': <Widget>[
+  '/skladki': <Widget>[
     SectionButton(icon: Icons.access_time_filled_sharp, title: 'Zyćko'),
     SectionButton(icon: Icons.account_balance_wallet, title: 'Unżyćko')
-  ]
+  ],
 };
