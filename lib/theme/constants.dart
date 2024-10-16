44fccd46f0f62e91a34c851d2fac8b8bdb63e9ec
praktykant3 <dawid.domaniecki2@gmail.com>
2024-10-16 08:31:44 +0200

    Updated constants.dart to contain dark theme colors

--- lib/theme/constants.dart
+++ lib/theme/constants.dart
@@ -7,7 +7,16 @@ var darkTheme = ThemeData.dark();
 // LIGHT THEME COLORS
 
 const kPrimaryLIGHT = Color(0xff8DDBE0);
-const kSecondaryLIGHT = Color(0xff9BC4BC);
-const kPrimaryLightLIGHT = Color(0xffD3FFE9);
-const kGray = Color(0xff4B5043);
-const kBlack = Color(0xff090909);
+const kSecondaryLIGHT = Color(0xffD3FFE9);
+const kTextPrimaryLIGHT = Color(0xff090909);
+const kTextSecondaryLIGHT = Color(0xff4B5043);
+const kBackgroundLIGHT = Color(0xff9BC4BC);
+
+
+// DARK THEME COLORS
+
+const kPrimaryDARK = Color(0xff222047);
+const kSecondaryDARK = Color(0xff16396D);
+const kTextPrimaryDARK = Color(0xffDFDFDF);
+const kTextSecondaryDARK = Color(0xffAAAAAA);
+const kBackgroundDARK = Color(0xff282C3A);
