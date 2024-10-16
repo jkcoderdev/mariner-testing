4930af08351924d9a1600875e47f48bebc0d5024
praktykant3 <dawid.domaniecki2@gmail.com>
2024-10-16 08:19:16 +0200

    Updated constants to contain light theme colors

--- lib/theme/constants.dart
+++ lib/theme/constants.dart
@@ -3,3 +3,11 @@ import 'package:flutter/material.dart';
 var lightTheme = ThemeData.light();
 
 var darkTheme = ThemeData.dark();
+
+// LIGHT THEME COLORS
+
+const kPrimaryLIGHT = Color(0xff8DDBE0);
+const kSecondaryLIGHT = Color(0xff9BC4BC);
+const kPrimaryLightLIGHT = Color(0xffD3FFE9);
+const kGray = Color(0xff4B5043);
+const kBlack = Color(0xff090909);
