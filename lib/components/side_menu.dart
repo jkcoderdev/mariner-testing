a6c390856d205f9d145989f33dad1804df9d9de0
Jan <20jackoza06@gmail.com>
2024-10-16 14:41:14 +0200

    Add menu page tiles

--- lib/components/side_menu.dart
+++ lib/components/side_menu.dart
@@ -2,20 +2,37 @@ import 'package:flutter/material.dart';
 
 import 'package:d360/theme/colors.dart';
 
+import 'package:d360/models/side_menu_tile_model.dart';
+
 class SideMenu extends StatelessWidget {
   const SideMenu({super.key});
 
   @override
   Widget build(BuildContext context) {
+    const List<SideMenuTileModel> routes = [
+      SideMenuTileModel(
+        title: 'Profil użytkownika',
+        icon: Icons.person
+      ),
+      SideMenuTileModel(
+          title: 'Ustawienia',
+          icon: Icons.settings
+      ),
+      SideMenuTileModel(
+          title: 'O aplikacji',
+          icon: Icons.info
+      ),
+    ];
+
     final colors = ThemeColors.of(context);
 
     return Drawer(
       backgroundColor: colors['background'],
-      child: ListView(
-        padding: EdgeInsets.zero,
+      child: Column(
         children: <Widget>[
           DrawerHeader(
             padding: const EdgeInsets.all(16.0),
+            margin: EdgeInsets.zero,
             decoration: BoxDecoration(
               color: colors['primary'],
             ),
@@ -55,6 +72,28 @@ class SideMenu extends StatelessWidget {
                 )
               ],
             )
+          ),
+          Expanded(
+            child: ListView.builder(
+              padding: EdgeInsets.zero,
+              itemCount: routes.length,
+              itemBuilder: (context, index) {
+                final route = routes[index];
+
+                return ListTile(
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  // visualDensity: const VisualDensity(vertical: -2.0),
+                  iconColor: colors['textPrimary'],
+                  leading: Icon(route.icon),
+                  title: Text(route.title),
+                  onTap: () {
+                    if (route.hasRoute) {
+                      Navigator.pushReplacementNamed(context, route.route!);
+                    }
+                  },
+                );
+              }
+            ),
           )
         ],
       ),
