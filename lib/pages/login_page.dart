5be04944b585d6d88b80f7b61d90b28ade034ba0
Jan <20jackoza06@gmail.com>
2024-10-16 10:53:10 +0200

    Add temporary logo to login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import 'package:d360/theme/colors.dart';
+
 import 'package:d360/components/form_button.dart';
 import 'package:d360/components/form_input.dart';
 
@@ -11,30 +13,40 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final colors = ThemeColors.of(context);
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Logowanie'), 
-      ),
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text(
-                'Proszę się zalogować',
-                style: TextStyle(fontSize: 20),
+              Text(
+                'MARINER',
+                style: TextStyle(
+                  fontSize: 48,
+                  fontWeight: FontWeight.w900,
+                  color: colors['textPrimary']
+                ),
               ),
-              const SizedBox(height: 20),
+              Text(
+                'Some subtitle text',
+                style: TextStyle(
+                  fontSize: 12,
+                  color: colors['textSecondary']
+                )
+              ),
+              const SizedBox(height: 32.0),
               const FormInput(
                 placeholder: 'Nazwa użytkownika',
               ),
-              const SizedBox(height: 10),
+              const SizedBox(height: 8.0),
               const FormInput(
                 placeholder: 'Hasło',
                 obscureText: true, 
               ),
-              const SizedBox(height: 20),
+              const SizedBox(height: 16.0),
               FormButton(
                 onPressed: () async {
                   final prefs = await SharedPreferences.getInstance();
