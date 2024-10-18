310b031add42c9538a7a4ea72b93ef9143dff54a
praktykant3 <dawid.domaniecki2@gmail.com>
2024-10-18 12:57:42 +0200

    Updated looks of user_detail.dart

--- lib/components/members_components/user_detail.dart
+++ lib/components/members_components/user_detail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mariner/theme/colors.dart';
 
 class UserDetail extends StatelessWidget {
   const UserDetail({super.key, required this.name, this.pesel = '* Nie podano *', this.email = '* Nie podano *', this.phone = '* Nie podano *'});
@@ -10,13 +11,18 @@ class UserDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final colors = ThemeColors.of(context);
+
     return AlertDialog(
+      backgroundColor: colors['secondary'],
         title: Text(name),
         content: SingleChildScrollView(
           child: ListBody(
             children: <Widget>[
               Text('PESEL: $pesel'),
+              SizedBox(height: 5.0,),
               Text('Email: $email'),
+              SizedBox(height: 5.0,),
               Text('nr tel: $phone'),
             ],
           ),
