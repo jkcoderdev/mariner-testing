7634e3d8a1a03830c3ac1eeb40362c9546fdfed1
jkcoderdev <20jackoza06@gmail.com>
2024-10-20 20:37:22 +0200

    Add module page widgets

--- lib/pages/module_section_page.dart
+++ lib/pages/module_section_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:auto_route/auto_route.dart';
 
+@RoutePage()
 class ModuleSectionPage extends StatelessWidget {
   const ModuleSectionPage({super.key});
 
