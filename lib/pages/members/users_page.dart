47c1703a43c420f8bf7242c39a1ae05d9ab63db3
jkcoderdev <20jackoza06@gmail.com>
2024-10-20 17:41:06 +0200

    Code clean up

--- lib/pages/members/users_page.dart
+++ lib/pages/members/users_page.dart
@@ -15,8 +15,10 @@ class UsersPage extends StatelessWidget {
     final colors = ThemeColors.of(context);
 
     return Scaffold(
-      appBar: AppBar(title: const Text('Użytkownicy') ,),
-      body: Padding(padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+      appBar: AppBar(
+        title: const Text('Użytkownicy'),
+      ),
+      body: Padding(padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -24,8 +26,8 @@ class UsersPage extends StatelessWidget {
             const User(name: 'ra2nek', phone: '+48 333 33 333', pesel: '20337472', email: 'ra4nek@gmail.com',),
             Divider(color: colors['special'],),
             const User(name: 'Andrzej', phone: '+55 242 422 22', pesel: '24221432',)
-        ],
-      ),
+          ],
+        ),
       )
     );
   }
