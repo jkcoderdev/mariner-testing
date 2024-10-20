5bc332e4d5be07bab40d6743f0e6bca65125878b
jkcoderdev <20jackoza06@gmail.com>
2024-10-20 20:37:03 +0200

    Add module page widgets

---
+++ lib/pages/module_menu_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:auto_route/auto_route.dart';
+
+@RoutePage()
+class ModuleMenuPage extends StatelessWidget {
+  const ModuleMenuPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/pages/module_section_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:auto_route/auto_route.dart';
+
+class ModuleSectionPage extends StatelessWidget {
+  const ModuleSectionPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
